597ab353923c901805bc1d449ee9dc850eaf16ae
APHOMER <AAPHOMER@GMAIL.COM>
2022-10-29 15:22:57 +0100

    my first HNG9 commit and this is stageone

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+
+app.get('/hngstageone', (req, res) => {
+    const data = {
+        slackUsername: 'APHOMER',
+        backend: true,
+        age: 26,
+        bio: 'My character is more important than my Learning'
+    };
+    const { slackUsername, backend, age, bio } = data;
+    // console.log(res.json(data));
+    return res.json(data);
+});
+
+
+
+app.listen(port, () => {
+    console.log(`HNG internship stage1  is working on port ${port}`)
+})
